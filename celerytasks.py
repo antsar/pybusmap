a08efdad6c6d636d1bcd018f6804d179b001597e
Anton Sarukhanov <code@ant.sr>
2015-11-06 20:51:39 -0500

    tack this onto last commit

---
+++ celerytasks.py
@@ -0,0 +1,21 @@
+from celery import Celery
+from datetime import timedelta
+
+from app import app
+
+celery = Celery(app.import_name, broker=app.config['CELERY_BROKER_URL'])
+celery.conf.update(app.config)
+TaskBase = celery.Task
+class ContextTask(TaskBase):
+    abstract = True
+    def __call__(self, *args, **kwargs):
+        with app.app_context():
+            return TaskBase.__call__(self, *args, **kwargs)
+celery.Task = ContextTask
+
+@celery.task()
+def get_routes(word):
+    # get bus routes from NextBus and store them in db
+    print("get_routes {0}".format(word))
+    return True;
+
