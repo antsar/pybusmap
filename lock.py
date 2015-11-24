85c654aca41677a61bb332d2c2f631f2a9f9d1c2
Anton Sarukhanov <code@ant.sr>
2015-11-24 18:35:25 -0500

    Lock cleanup

--- lock.py
+++ lock.py
@@ -12,73 +12,97 @@ class Lock():
     Synchronization is bad! Use at your own risk.
     """
 
-    def __init__(self, key, shared=False, expires=25, timeout=30, step=0.5):
+    def __init__(cls, key, shared=False, expires=25, timeout=30, step=0.5):
         """
-        Redis lock.
-        key = name of the lock
-        expires = lock expiration time in seconds. in case of failed jobs.
-        timeout = max time to wait for a lock
+        A lock mechanism, using Redis as a backend.
+
+        key = identifier for this lock
+        expires = lock expiration time in seconds (when it is considered stale)
+        timeout = max time to wait for a lock to become available
+
+        Multiple shared locks can exist simultaneously.
+        Only one non-shared (exclusive) lock can exist at a time.
+        Shared locks wait for exclusive locks to release.
+        If an exclusive lock is set, new shared lock attempts will wait (block).
+        Similarly, exclusive locks will wait (block) until all shared locks to clear.
         """
 
-        self.exclusive_key = "bm-lock-x-{0}".format(key)
-        self.shared_key = "bm-lock-s-{0}".format(key)
-        self.shared = shared
-        self.expires = expires
-        self.timeout = timeout
-        self.step = step
-        self.pid = os.getpid()
+        cls.exclusive_key = "bm-lock-x-{0}".format(key)
+        cls.shared_key = "bm-lock-s-{0}".format(key)
+        cls.shared = shared
+        cls.expires = expires
+        cls.timeout = timeout
+        cls.step = step
+        cls.pid = os.getpid()
 
 
-    def __enter__(self):
-        while self.timeout >= 0:
-            self.expires = time() + self.expires + 1
-            if self.shared == False:
-                # We're getting an exclusive lock
-                if r.setnx(self.exclusive_key, pickle.dumps((self.expires,self.pid))):
-                    # Wait for all shared locks to clear, then proceed
-                    self._wait_for_shared()
+    def __enter__(cls):
+        """
+        Attempt to acquire the lock.
+        If the lock is unavailable, retry for `timeout` seconds.
+        If a stale lock is found (older than `expires`), remove it.
+        """
+        while cls.timeout >= 0:
+            cls.expires = time() + cls.expires + 1
+            if cls.shared:
+                # Make sure nobody has exclusive, but don't take it.
+                if not r.get(cls.exclusive_key):
+                    # Nobody has exclusive. Get our shared lock
+                    cls._set_shared()
                     return
             else:
-                # Make sure nobody has exclusive, but don't take it.
-                if not r.get(self.exclusive_key):
-                    # Nobody has exclusive. Get our shared lock
-                    self._get_shared()
+                # We're getting an exclusive lock. Set it.
+                if cls._set_exclusive():
+                    # Some shared locks may still exist. Wait for them to release.
+                    cls._wait_for_shared()
                     return
             # Lock not aquired! Check for stale exclusive lock
-            oldlock = r.get(self.exclusive_key)
+            oldlock = r.get(cls.exclusive_key)
             (existing_expires, existing_pid) = pickle.loads(oldlock)
             if existing_expires and float(existing_expires) < time():
                 # Stale Exc Lock found. Delete it.
-                r.delete(self.exclusive_key)
+                r.delete(cls.exclusive_key)
             # Tick and repeat until timeout.
             if existing_expires:
                 remaining = existing_expires - time()
-            self.timeout -= self.step
-            sleep(self.step)
+            cls.timeout -= cls.step
+            sleep(cls.step)
         # Timed out
-        raise(LockException("Could not acquire lock: {0}".format(self.exclusive_key)))
+        raise(LockException("Could not acquire lock: {0}".format(cls.exclusive_key)))
 
-    def __exit__(self, typ, value, traceback):
-        if self.shared:
-            r.lrem(self.shared_key, 0, pickle.dumps((self.expires, self.pid)))
+    def __exit__(cls, typ, value, traceback):
+        """
+        Release the lock.
+        """
+        if cls.shared:
+            r.lrem(cls.shared_key, 0, pickle.dumps((cls.expires, cls.pid)))
         else:
-            r.delete(self.exclusive_key)
+            r.delete(cls.exclusive_key)
 
-    def _get_shared(self):
-        r.lpush(self.shared_key, pickle.dumps((self.expires, self.pid)))
+    def _set_shared(cls):
+        """
+        Set a shared lock
+        """
+        return r.lpush(cls.shared_key, pickle.dumps((cls.expires, cls.pid)))
 
-    def _wait_for_shared(self):
-        while r.llen(self.shared_key) > 0 and self.timeout >= 0:
-            for sk in r.lrange(self.shared_key, 0, -1):
-                (self.expires, self.pid) = pickle.loads(sk)
-                if float(self.expires) < time():
-                    r.lrem(self.shared_key, 0, pickle.dumps((self.expires, self.pid)))
-                self.timeout -= self.step
-                sleep(self.step)
-        if r.llen(self.shared_key) == 0:
+    def _set_exclusive(cls):
+        """
+        Set an exclusive lock
+        """
+        return r.setnx(cls.exclusive_key, pickle.dumps((cls.expires,cls.pid)))
+
+    def _wait_for_shared(cls):
+        while r.llen(cls.shared_key) > 0 and cls.timeout >= 0:
+            for sk in r.lrange(cls.shared_key, 0, -1):
+                (cls.expires, cls.pid) = pickle.loads(sk)
+                if float(cls.expires) < time():
+                    r.lrem(cls.shared_key, 0, pickle.dumps((cls.expires, cls.pid)))
+                cls.timeout -= cls.step
+                sleep(cls.step)
+        if r.llen(cls.shared_key) == 0:
             return
         else:
-            raise(LockException("Shared locks still present: {0}".format(self.shared_key)))
+            raise(LockException("Shared locks still present: {0}".format(cls.shared_key)))
 
 class LockException(Exception):
     pass
